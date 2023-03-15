6b05b7f9f21d4d06fc0b7627027ae69e2767fec7
Solon <amaral.solon@hotmail.com>
2023-03-15 16:19:02 -0300

    Req 1 - Test Class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "leite",
+        "parmalat",
+        "02/02/02",
+        "02/03/02",
+        1234,
+        "manter resfriado",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "leite"
+    assert product.nome_da_empresa == "parmalat"
+    assert product.data_de_fabricacao == "02/02/02"
+    assert product.data_de_validade == "02/03/02"
+    assert product.numero_de_serie == 1234
+    assert product.instrucoes_de_armazenamento == "manter resfriado"
