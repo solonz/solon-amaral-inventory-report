c7229cb59881cad9865edd8fdf2a37d797ec382c
Solon <amaral.solon@hotmail.com>
2023-03-17 17:19:27 -0300

    Req 8 - Test Product Report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # https://www.programiz.com/python-programming/methods/built-in/repr
+    # https://www.geeksforgeeks.org/python-repr-function/
+    produto = Product(
+        id=1,
+        nome_do_produto="farinha",
+        nome_da_empresa="Farinini",
+        data_de_fabricacao="01-05-2021",
+        data_de_validade="02-06-2023",
+        numero_de_serie="123456",
+        instrucoes_de_armazenamento="ao abrigo de luz",
+    )
+    report = (
+        "O produto farinha fabricado em 01-05-2021"
+        " por Farinini com validade até 02-06-2023"
+        " precisa ser armazenado ao abrigo de luz."
+    )
+    assert repr(produto) == report
